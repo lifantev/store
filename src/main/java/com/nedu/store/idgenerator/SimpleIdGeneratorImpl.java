805e14a1f858cde83302480f13607a709fe912ac
lifantev <lifantev.da@phystech.edu>
2021-07-21 19:49:33 +0300

    Prototype scope for IdGenerator.

--- src/main/java/com/nedu/store/idgenerator/SimpleIdGeneratorImpl.java
+++ src/main/java/com/nedu/store/idgenerator/SimpleIdGeneratorImpl.java
@@ -1,8 +1,10 @@
 package com.nedu.store.idgenerator;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component("idGenerator")
+@Scope("prototype")
 public class SimpleIdGeneratorImpl implements IdGenerator {
     private long id;
 
